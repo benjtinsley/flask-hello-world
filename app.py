636a58c1559bad1afd985a0aa98ae6e645cca30a
benjtinsley <benj.tinsley@gmail.com>
2024-03-22 14:54:02 -0600

    connects to render db

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from BD Tinsley in 3308!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://demo_flask_db_user:Btfg95mH0oYuk4dKUWJPQjCXuVAMVBfm@dpg-cnuul77109ks73ejf7pg-a.oregon-postgres.render.com/demo_flask_db")
+    conn.close()
+    return 'Database Connection Successful'
